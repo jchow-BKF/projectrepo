31da63a94c5bea64cb073a8db2824a804b1534ef
John <chwjohn18e@gmai.com>
2026-04-20 18:00:36 +0800

    changes on collaborator

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ This is a Read me file for introductory content
 changes on the main branch 
 This is the featurex branch modification
 
+Add change for collaborator
--- feature.py
+++ feature.py
@@ -1,11 +1,8 @@
 Feature
 
-<<<<<<< HEAD
+<
 var result = sum(4, 15)
  console.log(result)
  
- 
-=======
-var result = sum(5, 10)
- console.log(result)
->>>>>>> featurex
+
+Add changes collaborator 
